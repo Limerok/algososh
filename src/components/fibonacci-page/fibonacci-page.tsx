33545969064cdd09d035bf50e5e4daa69ad276be
Alexei Slautin <slaytin2014@yandex.ru>
2024-01-18 15:42:13 +0300

    finish string and fibonacci page

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,44 +4,62 @@ import styles from './fibonacci.module.css';
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { sleep } from "../../utils/utils";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { useForm } from "../../hooks/useForm";
 
 export const FibonacciPage: React.FC = () => {
-  const [input, setInput] = useState<string>('');
   const [arr, setArr] = useState<Array<string>>([])
+  const [buttonPressed, setButtonPressed] = useState<boolean>(false)
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
+  const maxNumber = 19;
+  const minNumber = 1;
 
-  const fibIterative = (n: number) => {
-    if (1 <= n && n <= 19) {
+  const { values, handleChange, setValues } = useForm({
+    input: ''
+  });
+
+  const getFibonacciNumbers = async (n: number) => {
+    setArr([]);
+    if (minNumber <= n && n <= maxNumber) {
       let tempArr: number[] = [0, 1];
+      setArr(['1'])
+      await sleep(SHORT_DELAY_IN_MS);
       for (let i = 2; i <= n + 1; i++) {
         tempArr.push(tempArr[i - 2] + tempArr[i - 1])
+        setArr(tempArr.slice(1).map((item) => item.toString()))
+        await sleep(SHORT_DELAY_IN_MS);
       }
-      tempArr.shift()
-      setArr(tempArr.map((item) => item.toString()))
-    } else {
-      return []
     }
+    setButtonPressed(false)
   }
 
   const calculate = () => {
-    fibIterative(+input)
+    setButtonPressed(true)
+    getFibonacciNumbers(+values.input)
   }
 
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
       <div className={styles.main}>
-        <Input type='!text' value={input} onChange={onChange} isLimitText={true} max={19} extraClass={styles.mr} />
-        <Button text="Рассчитать" onClick={calculate} />
+        <Input
+          name='input'
+          type='number'
+          value={values.input}
+          onChange={handleChange}
+          isLimitText={true}
+          max={maxNumber}
+          extraClass={styles.mr}
+        />
+        <Button
+          text="Рассчитать"
+          onClick={calculate}
+          isLoader={buttonPressed}
+          disabled={values.input === '' || !/^\d+$/.test(values.input) || +values.input > 19}
+        />
       </div>
       <div className={styles.letters}>
-        {
-          arr.map((item, index) =>
-            <Circle index={index} letter={item}></Circle>
-          )
-        }
+        {arr.map((item, index) => <Circle key={index} index={index} letter={item} />)}
       </div>
     </SolutionLayout>
   );
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,35 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from './string.module.css';
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { useForm } from "../../hooks/useForm";
+import { sleep, swap } from "../../utils/utils";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const StringComponent: React.FC = () => {
-  const [input, setInput] = useState<string>('');
-  const [arr, setArr] = useState<Array<string>>([])
+  const [arr, setArr] = useState<Array<{ value: string, id: number, state: ElementStates }>>([])
+  const [buttonPressed, setButtonPressed] = useState<boolean>(false)
+  let isFirstCall = useRef(true);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
+  const maxLength = 11;
+
+  const { values, handleChange, setValues } = useForm({
+    input: ''
+  });
 
   const addWord = () => {
-    setArr(input.split(''))
-    console.log(arr)
+    setButtonPressed(true)
+    isFirstCall.current = true
+    setArr(values.input.split('').map((letter, index) => {
+      return { id: index + 1, value: letter, state: ElementStates.Default }
+    }))
+  }
+
+  useEffect(() => {
+    if (isFirstCall.current) {
+      isFirstCall.current = false
+      reverseWord()
+    }
+  }, [arr])
+
+  const reverseWord = async () => {
+    let tempArr = arr;
+    let start = 0;
+    let end = tempArr.length - 1
+    while (start < end) {
+      tempArr[start].state = ElementStates.Changing
+      tempArr[end].state = ElementStates.Changing
+      setArr([...tempArr]);
+      await sleep(SHORT_DELAY_IN_MS);
+      swap(tempArr, start, end)
+      tempArr[start].state = ElementStates.Modified
+      tempArr[end].state = ElementStates.Modified
+      start++;
+      end--;
+      await sleep(SHORT_DELAY_IN_MS);
+      setArr([...tempArr]);
+    }
+    if (tempArr.length !== 0) {
+      tempArr[start].state = ElementStates.Modified
+      setArr([...tempArr]);
+    }
+    setButtonPressed(false)
   }
 
   return (
     <SolutionLayout title="Строка">
-      <form className={styles.main}> 
-        <Input type='text' value={input} onChange={onChange} isLimitText={true} maxLength={11} extraClass={styles.mr} />
-        <Button text="Развернуть" onClick={addWord}/>
-      </form>
+      <div className={styles.main}>
+        <Input
+          name='input'
+          type='text'
+          value={values.input}
+          onChange={handleChange}
+          isLimitText={true}
+          maxLength={maxLength}
+          extraClass={styles.mr}
+        />
+        <Button
+          text="Развернуть"
+          onClick={addWord}
+          isLoader={buttonPressed}
+          disabled={buttonPressed || values.input === ''}
+        />
+      </div>
       <div className={styles.letters}>
-        {
-          arr.map((item) => 
-            <Circle letter={item}></Circle>
-          ) 
-        }
+        {arr.map((item) => <Circle state={item.state} key={item.id} letter={item.value} />)}
       </div>
     </SolutionLayout>
   );
